de77068658d8f3037a7b2438184c7ed2ce88996b
mimpushkarev <107047559+mimpushkarev@users.noreply.github.com>
2023-01-24 20:16:39 +0000

    review fix

--- app/types.ts
+++ app/types.ts
@@ -1,4 +1,4 @@
-import { FC, memo, PropsWithChildren } from "react";
+import { FC, PropsWithChildren } from "react";
 
 type RootLayoutPropsType = PropsWithChildren<{}>;
 export type RootLayoutType = FC<RootLayoutPropsType>;
