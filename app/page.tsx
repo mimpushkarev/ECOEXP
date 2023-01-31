d391f346266f429103ba66bf603aefe916d09b7e
AnnaErs <84527125+AnnaErs@users.noreply.github.com>
2023-01-31 16:09:17 +0500

    BIKDARTEM-6: [FRONT] Создать секцию «Главная» (#4)
    * BIKDARTEM-6: [FRONT] Создать секцию «Главная»
    
    ---------
    
    Co-authored-by: AnnaErs <annaershova02@gamil.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import {memo} from "react";
 
+import MainSection from "@components/sections/MainSection/MainSection";
 import {MainPageType} from "./types";
 
 const MainPage: MainPageType = () => {
   return (
     <>
-      <h1>Главная</h1>
+      <MainSection />
     </>
   );
 };
