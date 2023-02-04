23351fc88e5fd5f97776ef97dbc3acc0b427c770
AnnaErs <annaershova02@gamil.com>
2023-02-04 23:18:15 +0500

    fix: BIKDARTEM-28: [FRONT] Стилизация и консолидация секций

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import {memo} from "react";
 
-import NavigationComponent from "@components/NavBar";
+import NavBar from "@components/NavBar";
 import Footer from "@components/Footer/Footer";
 
 import "@assets/styles/globalLayout.scss";
@@ -11,7 +11,7 @@ const RootLayout: RootLayoutType = ({children}) => {
   return (
     <html lang="en">
       <body>
-        <NavigationComponent />
+        <NavBar />
         {children}
         <Footer />
       </body>
--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,16 +1,23 @@
+"use client";
 import {memo} from "react";
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 import styles from "./NavBar.module.scss";
 
 const NavBar = () => {
+  const router = usePathname();
   return (
-    <div className={styles.navbar}>
+    <div className={router === "/" ? styles.navbarMainSection : styles.navbar}>
       <div className="container">
         <div className={styles.navbarWrapper}>
-          <Link href="/" passHref legacyBehavior>
-            <a className={["H4", styles.link].join(" ")}>Главная</a>
-          </Link>
+          {router === "/" ? (
+            <div className={styles.link} />
+          ) : (
+            <Link href="/" passHref legacyBehavior>
+              <a className={["H4", styles.link].join(" ")}>Главная</a>
+            </Link>
+          )}
           <div className={styles.nav}>
             <Link href="/about-us" passHref legacyBehavior>
               <a className={["H4", styles.link].join(" ")}>О нас</a>
@@ -29,3 +36,19 @@ const NavBar = () => {
 };
 
 export default memo(NavBar);
+
+// const navigation = [
+//   {id: 1, title: "Главная", path: "/"},
+//   {id: 2, title: "О нас", path: "/about-us"},
+//   {id: 3, title: "Тарифы", path: "/tariffs"},
+//   {id: 4, title: "Контакты", path: "/contacts"},
+// ];
+{
+  /* <div>
+  {navigation.map(({id, title, path}) => (
+    <Link key={id} href={path} passHref legacyBehavior>
+      <a className={["H4", styles.link].join(" ")}>{title}</a>
+    </Link>
+  ))}
+</div> */
+}
